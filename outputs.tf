56a1744e5d516e89fac3fb058361aa0aaba4391e
Justin Diaz <jdiaz@kwazi.io>
2023-12-21 15:33:09 -0500

    Updated Outputs

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "arn" {
 output "egress_rules" {
   description = "List of Egress Rule IDs Associated w/ this Firewall (by this Module)"
   
-  value = concat([
+  value = flatten(concat(
     aws_vpc_security_group_egress_rule.eks_cluster_all_ipv4.*.id,
     aws_vpc_security_group_egress_rule.eks_cluster_all_ipv6.*.id,
-  ])
+  ))
 }
 
 output "id" {
@@ -24,9 +24,9 @@ output "id" {
 output "ingress_rules" {
   description = "List of Ingress Rule IDs Associated w/ this Firewall (by this Module)"
   
-  value = concat([
+  value = flatten(concat(
     [aws_vpc_security_group_ingress_rule.eks_cluster_all_self.id],
-  ])
+  ))
 }
 
 output "network_id" {
